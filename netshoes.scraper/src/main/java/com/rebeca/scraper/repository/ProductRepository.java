a6641eb386801efdf6c3071fa9b1d1c7a8f2f1d5
Rebeca <rebecadasilva.v3@gmail.com>
2026-04-20 15:18:19 -0300

    feat: criar repositório JPA para Product.

--- netshoes.scraper/src/main/java/com/rebeca/scraper/repository/ProductRepository.java
+++ netshoes.scraper/src/main/java/com/rebeca/scraper/repository/ProductRepository.java
@@ -1,4 +1,12 @@
 package com.rebeca.scraper.repository;
 
-public class ProductRepository {
+import com.rebeca.scraper.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**Interface de acesso a dados para a entidade Product.*/
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
